9477d04d98b2a81f30c312d054f8ea4c1d36b05b
Veli-V <vaisanenjj@gmail.com>
2017-02-14 19:56:09 +0200

    All configs to ini file, fixed some problems with watch

--- sniffer.py
+++ sniffer.py
@@ -1,14 +1,42 @@
 #! /usr/bin/env python
 from scapy.all import *
 from subprocess import call
+import ConfigParser
 
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+
+# read config
+Config = ConfigParser.ConfigParser()
+Config.read("config.ini")
+
+mac= ConfigSectionMap("dashButton")['mac']
+ip = ConfigSectionMap("dashButton")['ip']
+print ip + " " + mac
+
+#callback function for sniffing.
 def arp_monitor_callback(pkt):
-    if pkt[ARP].hwsrc == '34:D2:70:DA:5D:9D':
-        print "Nappi1!"
-    if pkt[ARP].psrc == '192.168.11.7':
+    # if dash button is recogniced. These should be right
+    # for some reasen hwsrc is not working
+    #if pkt[ARP].hwsrc == mac:
+        #print "Nappi1!"
+    if pkt[ARP].psrc == ip:
         print "nappi!"
-        # call('./testi.sh', shell=True)
+        call('./watch.py', shell=True)
+    #this is here for debugging purposes, prints all arp calls out.
     if ARP in pkt and pkt[ARP].op in (1,2): #who-has or is-at
         return pkt.sprintf("%ARP.hwsrc% %ARP.psrc%")
 
+#Sniff, store=0 so doesn't use memory
 sniff(prn=arp_monitor_callback, filter="arp", store=0)
--- watch.py
+++ watch.py
@@ -3,21 +3,54 @@
 from os import listdir
 import random
 from subprocess import call
+import ConfigParser
 
-dirpath="/home/veli-v/Downloads/tuho"
-files=listdir( dirpath )
+
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+
+# read config
+Config = ConfigParser.ConfigParser()
+Config.read("config.ini")
+
+videodir= ConfigSectionMap("files")['videodir']
+subdirs = ConfigSectionMap("files")['subdirs']
+# path from where files should be fetched
+#dirpath="/home/veli-v/Downloads/tuho"
+
+videodir = videodir +'Family.Guy.S10'
+print videodir
+print subdirs
+# Files to array
+files=listdir( videodir )
+
+# Debug print all files
 for file in files:
-	print file
+    print file
 print len(files)
 
-filePlace = random.randint(0,6)
-print filePlace
-print files[filePlace]
+# Randomize file
+#filePlace = random.randint(0,6)
+#print filePlace
+#print files[filePlace]
 
-command="mplayer "+dirpath+"/"+files[filePlace]
-print command
-call(command, shell=True) 
-command="mv "+dirpath+"/"+files[filePlace]+" "+dirpath+"/watched"
-print command
+# Watch the file
+#command="mplayer"+videodir+"/"+files[filePlace]
+#print command
+#call(command, shell=True) 
 
-call(command, shell=True) 
+# After watching move to watched folder so it does not come again.
+#command="mv "+videodir+"/"+files[filePlace]+" "+dirpath+"/watched"
+#print command
+#call(command, shell=True) 
